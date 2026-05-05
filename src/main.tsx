8ebf3f3d29ebbbe185a280d3d7462fe290d86813
arcjejulab <bulmyul89@gmail.com>
2026-05-06 05:13:04 +0900

    인트로 페이지 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import IntroOverlay from './IntroOverlay';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <>
-      <IntroOverlay />
       <App />
-    </>
   </StrictMode>,
 );
