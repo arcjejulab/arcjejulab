7cc137e6e4159c5a85c698c722e1e2b280facc24
arcjejulab <bulmyul89@gmail.com>
2026-05-08 14:39:37 +0900

    Update Login.tsx

--- src/pages/Admin/Login.tsx
+++ src/pages/Admin/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../lib/firebase'; // 아까 만든 설정파일 연결
+import { auth } from '../../firebase'; // 아까 만든 설정파일 연결
 
 const AdminLogin = () => {
   const [email, setEmail] = useState('');
