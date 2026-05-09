6d3b62925ed032c6f0b290932410f4ac6c022822
arcjejulab <bulmyul89@gmail.com>
2026-05-09 21:18:54 +0900

    Fix Firebase Auth config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzrjiTtQU3eXUqXiWroCFbq_hbioPbxSw",
+  apiKey: "AIzaSyAzrj1ItQU3eXUqXiwRoCFbq_hbioPbxSw",
   authDomain: "allrounder-admi.firebaseapp.com",
   projectId: "allrounder-admi",
   storageBucket: "allrounder-admi.firebasestorage.app",
@@ -12,9 +14,13 @@ const firebaseConfig = {
   measurementId: "G-259H8JXTGG"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
 const auth = getAuth(app);
 
+// Initialize Analytics only when supported
 isSupported().then((supported) => {
   if (supported) {
     getAnalytics(app);
