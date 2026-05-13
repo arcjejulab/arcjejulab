4da7214468236e0d09e859b1aab3fed90364105e
arcjejulab <bulmyul89@gmail.com>
2026-05-13 19:36:59 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const ArcLogo = ({ isDark = false }: { isDark?: boolean }) => (
     <img
       src="/public/logo.png"
       alt="Allrounder Coffee Lab"
-      className={`h-18 w-auto object-contain md:h-22 ${isDark ? 'brightness-0 invert' : ''}`}
+      className={`h-16 w-auto object-contain md:h-20 ${isDark ? 'brightness-0 invert' : ''}`}
       draggable={false}
     />
   </button>
